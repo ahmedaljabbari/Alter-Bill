bdfcf94844015d047819628308d5554cfd89e982
Ahmed-hr <ahmedaljabbari.hr@gmail.com>
2022-05-01 12:53:05 +0300

    Lesson 21 - Delete Document

--- app.js
+++ app.js
@@ -57,3 +57,20 @@ app.post('/bills', (req, res) => {
       res.status(500).json({err: 'Could not create new document'})
     })
 })
+
+app.delete('/bills/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+
+    db.collection('alter-bills')
+      .deleteOne({_id: new ObjectId(req.params.id)})
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not delete the document'})
+      })
+      
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+})
